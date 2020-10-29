26c8c80d111201895310cf557b7c96454cc5dea7
softelevation <vsandhu@softelevation.com>
2020-10-29 23:05:48 +0530

    push code

--- src/app/page/people-around/people-around.component.ts
+++ src/app/page/people-around/people-around.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { VueService } from './../vue.service';
 
 @Component({
   selector: 'app-people-around',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleAroundComponent implements OnInit {
 
-  constructor() { }
+  lisabeyu_image: string;
+  cyruskind_image: string;
+  lindseymallon_image: string;
+  ardilladeneys_image: string;
+  zeejacattack_image: string;
+  officialkatka_image: string;
+  nu__dao_image: string;
+  ianastasiagreen_image: string;
+
+  constructor(private vueService: VueService) { }
 
   ngOnInit(): void {
+
+    this.vueService.instaApi('lisabeyu').subscribe(res => {
+        this.lisabeyu_image = res.graphql.user.profile_pic_url;
+    });
+
+    this.vueService.instaApi('cyruskind').subscribe(res => {
+      this.cyruskind_image = res.graphql.user.profile_pic_url;
+    });
+
+    this.vueService.instaApi('_lindseymallon').subscribe(res => {
+      this.lindseymallon_image = res.graphql.user.profile_pic_url;
+    });
+
+    this.vueService.instaApi('ardilladeneys').subscribe(res => {
+      this.ardilladeneys_image = res.graphql.user.profile_pic_url;
+    });
+
+    this.vueService.instaApi('zeejacattack').subscribe(res => {
+      this.zeejacattack_image = res.graphql.user.profile_pic_url;
+    });
+
+    this.vueService.instaApi('officialkatka').subscribe(res => {
+      this.officialkatka_image = res.graphql.user.profile_pic_url;
+    });
+
+    this.vueService.instaApi('nu__dao').subscribe(res => {
+      this.nu__dao_image = res.graphql.user.profile_pic_url;
+    });
+
+    this.vueService.instaApi('ianastasiagreen').subscribe(res => {
+      this.ianastasiagreen_image = res.graphql.user.profile_pic_url;
+    });
+    
+
   }
 
 }
